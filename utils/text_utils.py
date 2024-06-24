51561640952e7776afb057416e407ba8ec0522ba
nickve28 <nickvaneyk@gmail.com>
2024-06-24 20:00:18 +0200

    account for capital letters

--- utils/text_utils.py
+++ utils/text_utils.py
@@ -1,6 +1,6 @@
 def to_cursive(text):
-    normal_map = "qwertyuiopasdfghjklzxcvbnm,.?12345678"
-    cursive_map = "𝑞𝑤𝑒𝑟𝑡𝑦𝑢𝑖𝑜𝑝𝑎𝑠𝑑𝑓𝑔ℎ𝑗𝑘𝑙𝑧𝑥𝑐𝑣𝑏𝑛𝑚,.?12345678"
+    normal_map = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM,.?12345678"
+    cursive_map = "𝑞𝑤𝑒𝑟𝑡𝑦𝑢𝑖𝑜𝑝𝑎𝑠𝑑𝑓𝑔ℎ𝑗𝑘𝑙𝑧𝑥𝑐𝑣𝑏𝑛𝑚𝑄𝑊𝐸𝑅𝑇𝑌𝑈𝐼𝑂𝑃𝐴𝑆𝐷𝐹𝐺𝐻𝐽𝐾𝐿𝑍𝑋𝐶𝑉𝐵𝑁𝑀,.?12345678"
 
     trans = str.maketrans(normal_map, cursive_map)
     return text.translate(trans)
