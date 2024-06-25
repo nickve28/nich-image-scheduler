1fef4f5293b2c895e45bac1fb7623c24fe09a136
Alexopus <alexopus.soft@gmail.com>
2024-06-25 18:41:56 +0200

    deviant schedule: add more  properties

---
+++ deviant_utils/pick_resolution.py
@@ -0,0 +1,13 @@
+from PIL import Image
+
+def get_optimal_resolution(image_path):
+    widths = [400, 600, 800, 900, 1024, 1280, 1600, 1920]
+    with Image.open(image_path) as img:
+        image_width = img.width
+
+    for i, width in enumerate(widths):
+        if width >= image_width:
+            return i + 1  # Add 1 because API values start at 1, not 0
+
+    # If all widths are smaller than the image, return the highest available
+    return len(widths)  # This will be 8 for 1920px
