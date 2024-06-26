7a84ee5ab199c603a5978a172815c29ad5a5e195
nickve28 <nickvaneyk@gmail.com>
2024-06-26 21:41:54 +0200

    refactor another constant

--- view_queue.py
+++ view_queue.py
@@ -3,6 +3,7 @@ import glob
 
 from utils.account import select_account
 from utils.cli_args import parse_arguments
+from utils.constants import QUEUE_TAG_MAPPING
 
 account = parse_arguments().account
 account_data = select_account(account)
@@ -16,15 +17,13 @@ platforms = account_data["platforms"]
 print(f"Checking queues for account {id}")
 print("\n")
 
-QUEUE_MAP = {"Twitter": "TWIT_Q", "Deviant": "DEVI_Q"}
-
 for platform in platforms:
     files = []
     print(f"Checking platform {platform}")
 
     platform_count = 0
     for extension in extensions:
-        matches = glob.glob(os.path.join(directory_path, f"*{QUEUE_MAP[platform]}*{extension}"))
+        matches = glob.glob(os.path.join(directory_path, f"*{QUEUE_TAG_MAPPING[platform]}*{extension}"))
         platform_count += len(matches)
         for file_path in matches:
             print(file_path)
